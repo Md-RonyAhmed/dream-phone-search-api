6798e08fa82ae7c9c9be5713804b071561282e9a
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-01 22:49:38 +0600

    load api successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+
+  // Handle Search button
+   const searchField = document.getElementById("search-field");
+
+   // Load Phone API
+   const loadPhones = () => {
+      const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
+      fetch(url)
+         .then((res) => res.json())
+         .then((data) => console.log(data));
+     };
+  
