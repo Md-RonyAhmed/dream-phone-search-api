19633d8d572e2d80ec68109791706655197ae265
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-02 00:24:48 +0600

    display phones div

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
+// Handle Search button
+const searchField = document.getElementById("search-field");
+// Load Phone API
+const loadPhones = () => {
+   const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
+   
+   // clear data
+    searchField.value = '';
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhones(data.data));
+};
 
-  // Handle Search button
-   const searchField = document.getElementById("search-field");
-
-   // Load Phone API
-   const loadPhones = () => {
-      const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
-      fetch(url)
-         .then((res) => res.json())
-         .then((data) => console.log(data));
-     };
-  
+//   Display phones
+const displayPhones = (phones) => {
+   const searchResult = document.getElementById("search-result");
+   searchResult.textContent = '';
+  phones.slice(0, 20).forEach((phone) => {
+   //   console.log(phone.phone_name);
+      const div = document.createElement('div');
+      div.classList.add('col');
+    div.innerHTML = `
+      <div class="card" style="width: 18rem;">
+  <img src="${phone.image}" class="card-img-top" alt="phone image">
+  <div class="card-body">
+    <h5 class="card-title">${phone.brand}</h5>
+    <h6 class="card-text">${phone.phone_name}</h6>
+    <button type="button" class="btn btn-primary">Show Details</button>
+  </div>
+</div>
+      `;
+      searchResult.appendChild(div);
+  });
+};
